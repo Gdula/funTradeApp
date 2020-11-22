e1b496e919c342d044d0fa1cad2438a1367c8445
Gdula <da.gdula@gmail.com>
2020-11-22 17:05:08 +0100

    Create project with spring initializr

---
+++ src/main/java/com/gdula/funTradeApp/FunTradeAppApplication.java
@@ -0,0 +1,13 @@
+package com.gdula.funTradeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunTradeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunTradeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdula/funTradeApp/FunTradeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdula.funTradeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunTradeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
